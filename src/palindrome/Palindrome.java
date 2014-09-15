7842ffe0acd701c2a686fda5d001eff25241eb72
Cory Gorham <Borham@Administrators-MacBook-Pro-4.local>
2014-09-15 01:51:27 -0400

    modified if statement to detect palindromes independent of case sensitivity

--- src/palindrome/Palindrome.java
+++ src/palindrome/Palindrome.java
@@ -17,23 +17,30 @@ public class Palindrome {
             int length;
 
             System.out.println("==SUPER PALINDROME CHECKER==");
+            System.out.println();
             System.out.println("Please enter a word, phrase, or whatever you want:");
             entry = scanIt.nextLine();
 
             for (int currentCharSpot = (entry.length() - 1); currentCharSpot >= 0; currentCharSpot--)
                 reverseEntry = reverseEntry + entry.charAt(currentCharSpot);
 
-            System.out.println("\"" + entry + "\" backwards is \"" + reverseEntry);
+            System.out.println();
+            System.out.println("\"" + entry + "\" backwards is \"" + reverseEntry + "\"");
+            System.out.println();
 
-            if (entry.equals(reverseEntry)) {
+            if ((entry.toLowerCase()).equals(reverseEntry.toLowerCase())) {
                 System.out.println("Congratulations.  You have yourself a palindrome.");
             }
             else {
                 System.out.println("Sorry.  You didn't enter a palindrome.");
             }
 
+            System.out.println();
+
         }while(repeater);
         System.out.println();
+        System.out.println();
+        System.out.println("Goodbye! :D");
 
     }
 }
